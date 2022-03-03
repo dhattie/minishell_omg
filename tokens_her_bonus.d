5ccf68490099643030c47a2e8bb8ed026717d827
dhattie <ikaneev@gmail.com>
2022-03-03 22:26:21 +0300

    03_03_22

--- tokens_her_bonus.d
+++
@@ -1,9 +0,0 @@
-tokens_her_bonus.o: src/tokens_her_bonus.c src/../include/minishell.h \
-  include/../libft/libft.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/readline.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/rlstdc.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/rltypedefs.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/keymaps.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/chardefs.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/tilde.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/history.h
--- wildcards_handler.d
+++
@@ -1,9 +0,0 @@
-wildcards_handler.o: src/wildcards_handler.c src/../include/minishell.h \
-  include/../libft/libft.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/readline.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/rlstdc.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/rltypedefs.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/keymaps.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/chardefs.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/tilde.h \
-  /Users/dhattie/.brew/Cellar/readline/8.1.2/include/readline/history.h
